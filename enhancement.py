189f5db75e36a94ab426268693a4106eed5a7fb7
Eashwar-S <eashwar.sathyamurthy@gmail.com>
2020-03-04 15:21:26 -0500

    Finished Implementing histogram equalization

--- enhancement.py
+++ enhancement.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-_,thresh = cv2.threshold(warpedtag,220,255,cv2.THRESH_BINARY)
+# _,thresh = cv2.threshold(warpedtag,220,255,cv2.THRESH_BINARY)
 
 def preprocessing(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -11,18 +11,70 @@ def preprocessing(img):
     edges = cv2.Canny(blur,100,200)
     return edges 
 
+def histogram(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    pixelNumber = gray.shape[0] * gray.shape[1]
+    # cv2.imshow("gray image",gray)
+    pixelDensity = np.zeros(256)
+
+    # pixelDensity[gray] += 1
+    for i in range(0, gray.shape[0]):
+        for j in range(0, gray.shape[1]):
+            pixelDensity[gray[i,j]] += 1
+    # print(pixelDensity)
+    # print('--------------')
+    # print(pixelNumber)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #      cv2.destroyAllWindows()
+    return pixelDensity, pixelNumber
+
+def histogramEqualization(pixelDensity, pixelNumber):
+    CDF = np.zeros(256)
+    for i in range(0,len(pixelDensity)):
+        for j in range(0, i+1):
+            CDF[i] += pixelDensity[j] / pixelNumber
+    # print('---------')
+    # print(CDF)
+    return CDF
+
+def newImage(gray, CDF):
+    for i in range(0, gray.shape[0]):
+        for j in range(0, gray.shape[1]):
+            gray[i,j] = np.round(CDF[gray[i,j]]*255)
+    
+
+# img = cv2.imread('Lena.png')
+# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# p,q = histogram(img)
+# # print(p)
+# plt.plot(np.arange(0,256,1),p)
+# plt.show()
+# cdf = histogramEqualization(p,q)
+# plt.plot(np.arange(0,256,1),cdf)
+# plt.show()
+# newImage(gray,cdf)
+
 
 ######################################################
 #              Reading Video 
 #####################################################
-cap = cv2.VideoCapture('./Night_Drive.mp4')
+cap = cv2.VideoCapture('NightDrive.mp4')
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     frame = cv2.resize(frame, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
-
-    processFrame(frame)
-
+    # p,q = histogram(frame)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    # print(p)
+    # plt.plot(np.arange(0,256,1),p)
+    # plt.show()
+    # cdf = histogramEqualization(p,q)
+    # # plt.plot(np.arange(0,256,1),cdf)
+    # # plt.show()
+    # newImage(gray,cdf)
+    # cv2.imshow("gray image",gray)
+    equ = cv2.equalizeHist(gray)
+    cv2.imshow("output",equ)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
